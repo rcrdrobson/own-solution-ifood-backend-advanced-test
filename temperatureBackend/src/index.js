69250a66b0c8c1f3a94e6bbb0d469e839f4331a3
Ricardo Robson <rcrdrobson@gmail.com>
2019-12-27 09:53:25 -0300

    Get temperature by city, lat and lon

--- temperatureBackend/src/index.js
+++ temperatureBackend/src/index.js
@@ -1,1 +1,48 @@
-var server = require("./server");
+const {server} = require("./server");
+const axios = require('axios');
+
+const appIdOpenWeatherMap = "01ebadfec7c7d5d219287c671877540c";
+
+server.get("/temperature", function(req, res){
+    if(req.query.city){
+        console.log("city")
+        getTemperatureByCity(req.query.city).then(temperature => {
+            console.log(temperature);
+            res.json({ temp: temperature})
+        });
+    }else if(req.query.lat && req.query.lon){
+        console.log("lat lon")
+        getTemperatureByLatlon(req.query.lat, req.query.lon).then(temperature => {
+            console.log(temperature);
+            res.json({ temp: temperature})
+        });
+    }
+});
+
+function getTemperatureByCity(city){
+    console.log("temp city")
+
+    return axios.get('http://api.openweathermap.org/data/2.5/weather?q='
+                        +city+'&units=metric&appid='+appIdOpenWeatherMap)
+        .then(function (response) {
+            return response.data.main.temp;
+        })
+        .catch(function (error) {
+            // TODO getting last temp availaible in database
+            console.log(error);
+        });
+}
+
+function getTemperatureByLatlon(lat, lon){
+    console.log("temp lat lon")
+
+    return axios.get('http://api.openweathermap.org/data/2.5/weather?lat='
+                        +lat+'&lon='+lon+'&units=metric&appid='+appIdOpenWeatherMap)
+        .then(function (response) {
+            return response.data.main.temp;
+        })
+        .catch(function (error) {
+            // TODO getting last temp availaible in database
+            console.log(error);
+        });
+}
--- temperatureBackend/src/server.js
+++ temperatureBackend/src/server.js
@@ -6,15 +6,6 @@ var port = 9090;
 server.use(bodyParser.urlencoded({ extended:true }));
 server.use(bodyParser.json());
 
-server.use(function(req, res, next){
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Content-Type', “application/json");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
-
 server.listen(port, function(){ console.log("Started at port "+port) });
 
 exports.server = server;
